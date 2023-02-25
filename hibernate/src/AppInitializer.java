867ccd06e2c74cd40de4a2307ece4b977e4e4954
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-25 14:41:37 +0530

    Start Project

--- hibernate/src/AppInitializer.java
+++ hibernate/src/AppInitializer.java
@@ -1,30 +1,5 @@
-import lk.ijse.hibernate.enitity.Customer;
-import lk.ijse.hibernate.util.FactoryConfiguration;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-
-/*
-    @author DanujaV
-    @created 24/05/2022 - 10:46 AM
-*/
 public class AppInitializer {
     public static void main(String[] args) {
-        Customer c1 = new Customer();
-        c1.setId("C001");
-        c1.setName("Kamal");
-        c1.setSalary(200000);
-        c1.setAddress("Panadura");
 
-        // get a new session
-        Session session = FactoryConfiguration.getInstance().getSession();
-
-        // transaction begin
-        Transaction transaction = session.beginTransaction();
-
-        session.save(c1);
-
-        transaction.commit();
-
-        session.close();
     }
 }
