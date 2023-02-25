0e72544f46d9e4cda2e08fb80a30f114fe5ee6da
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-25 15:10:32 +0530

    complete code FactoryConfigeration class

--- hibernate/src/lk/ijse/hibernate/util/FactoryConfigeration.java
+++ hibernate/src/lk/ijse/hibernate/util/FactoryConfigeration.java
@@ -1,4 +1,24 @@
 package lk.ijse.hibernate.util;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class FactoryConfigeration {
+    private static FactoryConfigeration factoryConfigeration;
+    private SessionFactory sessionFactory;
+
+    private FactoryConfigeration(){
+        Configuration configuration = new Configuration().configure();
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
+    public static FactoryConfigeration getInstance(){
+        return (factoryConfigeration==null)? factoryConfigeration=new FactoryConfigeration()
+                :factoryConfigeration;
+    }
+
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
 }
